ebd44a3fad06548e73eb1c9a5e901235029bab7a
matthieu pereira <matthieu.pereira@etu.unistra.fr>
2020-11-09 23:03:33 +0100

    updated main

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,33 @@
 #include "Map.h"
 #include "GeometricForms.h"
+#include "SFMLDrawer.h"
+#include <SFML/Graphics.hpp>
 
 
 int main(int argc, char const *argv[])
 {
-    Map map(100,100);
-    map.addCircle(0,0,5);
-    map.addLine(0,5,4.2);
-    point center;
-    center.x=4;center.y=2;
-    map.addRectangle(center, 4.3,2.1);
-
-    map.printAllForms();
-
+    Map map(1000,1000);
+   SFMLDrawer drawer(map);
+   drawer.addCircle(point{500,500}, 200);
+   drawer.addRectangle(point{100,100}, 50,50);
+   drawer.addLine(point{700,900}, 1000);
     
+    sf::RenderWindow window(sf::VideoMode(1000, 1000), "SFML works!");
+
+
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+        window.clear();
+        drawer.drawAll(window);
+        window.display();
+    }
 
 
 
