63841b873eaa54a3ba7a820c142b87bf9864c15e
matthieu pereira <matthieu.pereira@etu.unistra.fr>
2020-11-09 16:41:22 +0100

    updated main

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 
 int main(int argc, char const *argv[])
 {
-    Map map;
+    Map map(100,100);
     map.addCircle(0,0,5);
     map.addLine(0,5,4.2);
     point center;
@@ -12,6 +12,11 @@ int main(int argc, char const *argv[])
     map.addRectangle(center, 4.3,2.1);
 
     map.printAllForms();
+
+    
+
+
+
     return 0;
 }
 
