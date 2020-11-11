10fa3620df879e64cb432dcd74b4876fdf11c6ae
matthieu pereira <matthieu.pereira@etu.unistra.fr>
2020-11-11 18:43:36 +0100

    added function to generate the base on pathManager

--- src/PathManager.cpp
+++ src/PathManager.cpp
@@ -1,10 +1,79 @@
 #include "PathManager.h"
 
 
-PathManager::PathManager(size_t pathLenP, size_t pathNumberP, float baseElementP) : pathLen(pathLenP), pathNumber(pathNumberP), baseElement(baseElementP)
+PathManager::PathManager(Map mapP, size_t pathLenP, size_t pathNumberP, float baseElementP)
+ : pathLen(pathLenP), pathNumber(pathNumberP), baseElement(baseElementP), map(mapP), distrib(1,5)
 {
 }
 
 PathManager::~PathManager()
 {
+    for (auto &&i : dnaList)
+    {
+        free(i);
+    }
+    
+}
+
+void PathManager::fillRandomPaths()
+{
+    dnaList.clear();
+    for (size_t i = 0; i < pathNumber; i++)
+    {
+        point* currentPath = (point*)malloc(pathLen*sizeof(point));
+        dnaList.push_back(currentPath);
+        // that's a copy
+        point currentPos = map.start;
+        for (size_t j = 0; j < pathLen; j++)
+        {
+            do
+            {
+                int roll = distrib(generator);
+                switch (roll)
+                {
+                case 1:
+                    currentPath[j] = point{0,0};
+                    break;
+                case 2:
+                    currentPath[j] = point{baseElement,0};
+                    break;
+                case 3:
+                    currentPath[j] = point{-baseElement,0};
+                    break;
+                case 4:
+                    currentPath[j] = point{0,baseElement};
+                    break;
+                case 5:
+                    currentPath[j] = point{0,-baseElement};
+                    break;
+
+                }
+                currentPos.x =+ currentPath[j].x;
+                currentPos.y =+ currentPath[j].y;
+            } while (map.isInObstacle(currentPos));
+            
+        }
+        
+    }
+    
+}
+
+
+void PathManager::printAllPaths()
+{
+    for (auto &&path : dnaList)
+    {
+        std:: cout << "-----------PATH---------";
+        for (size_t i = 0; i < pathLen; i++)
+        {
+            std::cout << "(" << path[i].x << "," << path[i].y<<")" << std::endl;
+        }
+        
+    }
+    
+}
+
+std::vector<point*> PathManager::getDnaList()
+{
+    return dnaList;
 }
--- src/PathManager.h
+++ src/PathManager.h
@@ -1,24 +1,38 @@
 #include <vector>
-#include <array>
+#include <random>
+
 
 #include "GeometricForms.h"
+#include "Map.h"
 
 
 class PathManager
 {
 private:
-size_t pathLen;
-size_t pathNumber;
+    size_t pathLen;
+    size_t pathNumber;
 
-// the distance (in x, y or both) of the elementary movement
-float baseElement;
+    // the distance (in x, y or both) of the elementary movement
+    float baseElement;
 
-std::vector<point*> adnList;
+    Map map;
 
-point evaluateEndPoint(point* path);
+    //vector of arrays
+    std::vector<point*> dnaList;
+    static point evaluateEndPoint(point* path);
+
+    std::default_random_engine generator;
+    std::uniform_int_distribution<int> distrib;
+
 
 public:
-    PathManager(size_t pathLen, size_t pathNumer, float baseElement);
+    PathManager(Map map, size_t pathLen, size_t pathNumer, float baseElement);
     ~PathManager();
 
+    void fillRandomPaths();
+    std::vector<point*> getDnaList();
+
+    //DBG
+    //DO NOT USE WITH A USABLE PATHS
+    void printAllPaths();
 };
