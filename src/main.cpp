1a26f20de393e99856a7e8e687887ea7d604a5cd
matthieu pereira <matthieu.pereira@etu.unistra.fr>
2020-11-23 13:22:35 +0100

    added support for command line arguments

--- src/main.cpp
+++ src/main.cpp
@@ -6,30 +6,69 @@
 #include "Path.h"
 
 #include <unistd.h>
+#include <sstream>
 
-int main()
+
+int main(int argc, char* argv[])
 {
+    point screenSize;
+    point mapSize;
+    point startPoint; point endPoint;
+    int pathLen;int pathNumber;
+    float baseElement;
+    float lineWidth;
+    int mutationChance;
+    if(argc == 1)
+    {
+        screenSize = {1920,1080};
+        mapSize = {1000,1000};
+        startPoint ={0,0}; endPoint ={1000,1000};
+        pathLen = 3000; pathNumber = 10;
+        baseElement = 10;
+        lineWidth = 10+0.1;
+        mutationChance = 4;
+    }else if (argc <13)
+    {
+        std::cout << "usage : " << argv[0] << " screenSizeX screenSizeY" 
+        "mapSizeX mapSizeY startPointX startPointY pathLength NumberOfPaths baseElement mutationChance" << std::endl;
+        exit(EXIT_FAILURE);
+    } else
+    {
+        std::istringstream(argv[1]) >>screenSize.x;
+        std::istringstream(argv[2]) >>screenSize.y;
+        std::istringstream(argv[3]) >>mapSize.x;
+        std::istringstream(argv[4]) >>mapSize.y;
+        std::istringstream(argv[5]) >>startPoint.x;
+        std::istringstream(argv[6]) >>startPoint.y;
+        std::istringstream(argv[7]) >>endPoint.x;
+        std::istringstream(argv[8]) >>endPoint.y;
+        std::istringstream(argv[9]) >>pathLen;
+        std::istringstream(argv[10]) >>pathNumber;
+        std::istringstream(argv[11]) >>baseElement;
+        std::istringstream(argv[12]) >>mutationChance;
+    }
+    
 
-    Map map(1000,1000);
-    PathManager manager(map, 2000, 10, 5);
 
-    SFMLDrawer drawer(map, manager);
+
+    sf::RenderWindow window(sf::VideoMode(screenSize.x, screenSize.y), "Genetic pathfinding!");
+
+
+    Map map(mapSize.x, mapSize.y, startPoint, endPoint, lineWidth);
+    PathManager manager(map, pathLen, pathNumber, baseElement, mutationChance);
+
+    SFMLDrawer drawer(map, manager, window);
     drawer.addRectangle(point{500,500}, 400,200);
     drawer.addLine(point{200,200}, 200);
     drawer.addLine(point{400,600}, 400);
     drawer.addCircle(point{300,100}, 90);
-    //drawer.addCircle(point{800,0}, 350);
+    drawer.addCircle(point{800,0}, 350);
 
 
     manager.fillRandomPaths();
 
-    std::cout << "\npathsFilled\n" << std::endl;
     manager.orderByScoreRandomed(); 
 
-
-    //exit(0);
-
-    sf::RenderWindow window(sf::VideoMode(1000, 1000), "SFML works!");
 
     int frameNumber=0;
     while (window.isOpen())
@@ -45,7 +84,7 @@ int main()
         manager.mutate();
 
         window.clear();
-        drawer.drawAll(window);
+        drawer.drawAll();
         window.display();
         std::cout << "\n\n\ndrawn frame : " << frameNumber << std::endl;
         frameNumber++;
